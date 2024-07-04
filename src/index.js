ddbb5f74aa5b369b19d2191729f38481555f4013
sarah-abraham <sarahabraham2014@gmail.com>
2024-07-04 20:39:55 +0530

    Added cards with tasks to the content section and button to add task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log("Linked");
 import {headerFunction} from './js/header.js';
 import { sidebarTitleFunction, sidebarBodyFunction} from './js/sidebar.js';
+import {createCard, createButton} from './js/content.js';
 import './styles/common.css';
 import './styles/header.css';
 
@@ -8,6 +9,7 @@ function component(){
     const header = document.querySelector(".header");
     header.appendChild(headerFunction());
 
+    
     const sidebar = document.querySelector(".sidebar");
     sidebar.appendChild(sidebarTitleFunction());
     sidebar.appendChild(sidebarBodyFunction("Default"));
@@ -24,6 +26,26 @@ function component(){
     sidebar.appendChild(sidebarBodyFunction("Today"));
     // sidebar.appendChild(sidebarBodyFunction("Default"));
     // sidebar.appendChild(sidebarBodyFunction("Today"));
+
+
+
+    const content = document.querySelector(".content");
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "card-container";
+    const buttonContainer = document.createElement("div");
+    buttonContainer.className = "button-container"
+    buttonContainer.appendChild(createButton());
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    cardContainer.appendChild(createCard("Go to market", "Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff. Go to market to buy stuff.", "HIGH", "03/07/2024"));
+    content.appendChild(cardContainer);
+    content.appendChild(buttonContainer);
 }
 
 document.body.appendChild(component());
---
+++ src/js/content.js
@@ -0,0 +1,53 @@
+import "../styles/content.css";
+import PlusIcon from "../assets/plus-circle-white.svg";
+
+function createCard(title, description, priority, due){
+    const card = document.createElement("div");
+    card.className = "card";
+    const h3Title = document.createElement("h3");
+    h3Title.textContent = "Title: "+title;
+
+    const descriptionContainer = document.createElement("div");
+    descriptionContainer.className = "description-container";
+    const h3Description = document.createElement("h3");
+    h3Description.textContent = "Description: "
+    const p = document.createElement("p");
+    p.textContent = description;
+    descriptionContainer.appendChild(h3Description);
+    descriptionContainer.appendChild(p);
+
+    const priorityContainer = document.createElement("div");
+    priorityContainer.className = "priority-container";
+    const h3Priority = document.createElement("h3");
+    h3Priority.textContent = "Priority: ";
+    const priorityText = document.createElement("p");
+    priorityText.textContent = priority;
+    priorityContainer.appendChild(h3Priority);
+    priorityContainer.appendChild(priorityText);
+
+    const date = document.createElement("div");
+    const h3Date = document.createElement("h3");
+    h3Date.textContent = "Due: "+due;
+    date.appendChild(h3Date);
+
+    card.appendChild(h3Title);
+    card.appendChild(descriptionContainer);
+    card.appendChild(priorityContainer);
+    card.appendChild(date);
+
+    return card
+}
+
+
+function createButton(){
+    const button = document.createElement("button");
+    button.className = "task-btn";
+    const plus = new Image();
+    plus.src = PlusIcon;
+    plus.className = "icon";
+    button.appendChild(plus);
+    const buttonText = document.createTextNode("Add new Task");
+    button.appendChild(buttonText);
+    return button;
+}
+export {createCard, createButton}
